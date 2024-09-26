c790a8b85bf3d22e885e2e07dcbe464d0919dacd
Igor Rzegocki <igor@rzegocki.pl>
2024-09-26 16:06:46 +0200

    feat(invidious): add liveness check for signature server

--- apps/invidious/entrypoint.sh
+++ apps/invidious/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+# shellcheck shell=ksh
 
 export POSTGRES_HOST="${POSTGRES_HOST:-$(yq '.db.host // ""' < /config/config.yml)}"
 export POSTGRES_PORT="${POSTGRES_PORT:-$(yq '.db.port //""' < /config/config.yml)}"
@@ -7,5 +8,13 @@ export POSTGRES_DB="${POSTGRES_DB:-$(yq '.db.dbname // ""' < /config/config.yml)
 export PGPASSWORD="${POSTGRES_PASS:-$(yq '.db.password //""' < /config/config.yml)}"
 
 sh /app/init-invidious-db.sh
+
+SIGNATURE_SERVER="$(yq '.signature_server' /app/config/config.yml)"
+
+if [ -n "${SIGNATURE_SERVER}" ] && [ "${SIGNATURE_SERVER}" != "null" ]; then
+  while ! sh -c "nc -z ${SIGNATURE_SERVER//:/ }"; do
+    echo "Checking if signature server ${SIGNATURE_SERVER} is alive..."
+  done
+fi
 
 exec "$@"
