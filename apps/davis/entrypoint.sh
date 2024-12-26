0958030bce8af3091dde52c2f2f027a795f4f927
Igor Rzegocki <igor@rzegocki.pl>
2024-12-26 11:16:09 +0100

    fix(davis): create webdav tmp dir

--- apps/davis/entrypoint.sh
+++ apps/davis/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+[ -n "${WEBDAV_HOMES_DIR}" ] && [ ! -d "${WEBDAV_HOMES_DIR}" ] && mkdir -p "${WEBDAV_HOMES_DIR}"
+[ -n "${WEBDAV_PUBLIC_DIR}" ] && [ ! -d "${WEBDAV_PUBLIC_DIR}" ] && mkdir -p "${WEBDAV_PUBLIC_DIR}"
+[ -n "${WEBDAV_TMP_DIR}" ] && [ ! -d "${WEBDAV_TMP_DIR}" ] && mkdir -p "${WEBDAV_TMP_DIR}"
 /usr/local/sbin/php-fpm &
 sleep 1
 
